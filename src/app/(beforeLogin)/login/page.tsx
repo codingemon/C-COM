912549d6958e669d44cc85236ee51993acc2c9b8
snoopy1224 <misty12024@gmail.com>
2023-12-22 20:17:36 +0900

    main.tsx 정리

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,7 +1,25 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function Login(): void {
+// import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import Main from "../_component/\bMain";
+
+export default function Login() {
   // url을 사용했더니 오류가 발생해서 url 제거
   // redirect(url : "/i/flow/login");
-  redirect("/i/flow/login");
+  // redirect("/i/flow/login");
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return (
+    // 배경을 똑같이 깔아줘서 해결
+    <Main />
+  );
 }
+
+// router.push
+// 뒤로 보내서 무한반복이 되버림
+// localhost:3000/ login -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// router.replace
+// 이동 후 이전 히스토리를 없애버림 무한루프(x)
+// localhost:3000/ login -> localhost:3000/login -> localhost:3000/i/flow/login
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,25 +1,5 @@
-import Image from "next/image";
-import styles from "../../app/page.module.css";
-import Link from "next/link";
-import cLogo from "/public/clogo.png";
+import Main from "./_component/\bMain";
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={cLogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정만들기
-        </Link>
-        <h3>이미 ChoiNet에 가입하셨나요?</h3>
-        <Link href="/i/flow/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }

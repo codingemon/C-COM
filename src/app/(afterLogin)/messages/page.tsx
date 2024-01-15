c724380eab80d8ee7e23f6c8bc360264deef5502
snoopy1224 <misty12024@gmail.com>
2024-01-15 14:34:37 +0900

    update:messages완성

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,42 +1,5 @@
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import "dayjs/locale/ko";
 import style from "./messages.module.css";
-import { faker } from "@faker-js/faker";
-
-dayjs.locale("ko");
-dayjs.extend(relativeTime);
-
-function Room() {
-  const user = {
-    id: "doraemon",
-    nickname: "도라에몽",
-    Messages: [
-      { content: "안녕하세요.", createdAt: new Date() },
-      { content: "안녕히가세요.", createdAt: new Date() },
-    ],
-  };
-  return (
-    <div className={style.room}>
-      <div className={style.roomUserImage}>
-        <img src={faker.image.avatar()} alt="" />
-      </div>
-      <div className={style.roomChatInfo}>
-        <div className={style.roomUserInfo}>
-          <b>{user.nickname} </b>
-          &nbsp;
-          <span>@{user.id}</span>· &nbsp;
-          <span className={style.postDate}>
-            {dayjs(user.Messages?.at(-1)?.createdAt).fromNow(true)}
-          </span>
-        </div>
-        <div className={style.roomLastChat}>
-          {user.Messages?.at(-1)?.content}
-        </div>
-      </div>
-    </div>
-  );
-}
+import Room from "./_component/Room";
 
 export default function Home() {
   return (

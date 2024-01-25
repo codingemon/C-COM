2e52e42f162870a7a64983c37c5bb028d55f9d66
snoopy1224 <misty12024@gmail.com>
2024-01-25 11:33:38 +0900

    update:로그인전후

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { auth } from "./auth";
 import { NextResponse } from "next/server";
 
+// 세션이 없으면 로그인으로 넘어가게
 export async function middleware() {
   const session = await auth();
   if (!session) {
